ba9995ea2ba06cfadc645f08857e38937a5aafe2
Eri Schon <erischon@gmail.com>
2020-10-02 10:28:18 +0200

    oc-p3 refactoring

--- helpmacgyver/macgyver.py
+++ helpmacgyver/macgyver.py
@@ -2,7 +2,7 @@ from .position import Position
 
 
 class MacGyver:
-    """ """
+    """ I'm the player. """
 
     def __init__(self, maze):
         """ Initialize position and bag. """
@@ -22,32 +22,32 @@ class MacGyver:
                 self.mg_pos = Position.up(self.mg_pos)
                 special_cell = self.maze.is_special_cell(self.mg_pos)
                 if special_cell is not False:
-                    self.what_special(special_cell)
+                    self.search_special(special_cell)
                 return True
         if direction == "DOWN":
             if Position.down(self.mg_pos) in self.paths:
                 self.mg_pos = Position.down(self.mg_pos)
                 special_cell = self.maze.is_special_cell(self.mg_pos)
                 if special_cell is not False:
-                    self.what_special(special_cell)
+                    self.search_special(special_cell)
                 return True
         if direction == "RIGHT":
             if Position.right(self.mg_pos) in self.paths:
                 self.mg_pos = Position.right(self.mg_pos)
                 special_cell = self.maze.is_special_cell(self.mg_pos)
                 if special_cell is not False:
-                    self.what_special(special_cell)
+                    self.search_special(special_cell)
                 return True
         if direction == "LEFT":
             if Position.left(self.mg_pos) in self.paths:
                 self.mg_pos = Position.left(self.mg_pos)
                 special_cell = self.maze.is_special_cell(self.mg_pos)
                 if special_cell is not False:
-                    self.what_special(special_cell)
+                    self.search_special(special_cell)
                 return True
 
-    def what_special(self, special_cell):
-        """ I'm looking if there is a something special on the cell. """
+    def search_special(self, special_cell):
+        """ I'm looking if there is something special on the cell. """
         if special_cell == "item1":
             self.pouch_bag(special_cell)
         elif special_cell == "item2":
